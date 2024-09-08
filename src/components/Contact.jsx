b6622acbcca5ec3d4e6fe1a1e4d77ffc9e8f0dc1
oddwes <oddwes777@gmail.com>
2024-09-08 14:02:42 -0400

    contact links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,36 @@
 import EmailIcon from '@mui/icons-material/Email'
 import GitHubIcon from "@mui/icons-material/GitHub"
 import Telegram from "@mui/icons-material/Telegram"
-import { Box } from "@mui/material"
+import { Box, Link } from "@mui/material"
 
 const Contact = () => {
+  const telegramLink = 'https://t.me/oddwes'
+  const githubLink = 'https://github.com/oddwes'
+  const emailAddress = 'oddwes3.0@gmail.com'
+
+  const socialLink = (link, component) => {
+    return (
+      <Box sx={{ padding: 1 }}>
+        <Link href={link} color="inherit" target="_blank" rel="noopener noreferrer">
+          {component}
+        </Link>
+      </Box>
+    )
+  }
+
+  const emailLink = () => {
+    return (
+      <Box sx={{ padding: 1 }}>
+        <Link
+          href={`mailto:${emailAddress}`}
+          color="inherit"
+        >
+          <EmailIcon />
+        </Link>
+      </Box>
+    )
+  }
+
   return (
     <Box
       sx={{
@@ -11,15 +38,9 @@ const Contact = () => {
         flexDirection: 'row'
       }}
     >
-      <Box sx={{ padding: 1 }}>
-        <Telegram />
-      </Box>
-      <Box sx={{ padding: 1 }}>
-        <GitHubIcon />
-      </Box>
-      <Box sx={{ padding: 1 }}>
-        <EmailIcon />
-      </Box>
+      {socialLink(telegramLink, <Telegram />)}
+      {socialLink(githubLink, <GitHubIcon />)}
+      {emailLink()}
     </Box>
   )
 }
